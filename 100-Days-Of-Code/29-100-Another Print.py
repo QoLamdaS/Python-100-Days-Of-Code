ad0dc5299d2a86e8b4cbbedf02caf4c9745bdbc2
NewBie=) <162775533+QoLamdaS@users.noreply.github.com>
2024-06-10 15:24:26 +0700

    Color generator function added

--- 100-Days-Of-Code/29-100-Another Print.py
+++ 100-Days-Of-Code/29-100-Another Print.py
@@ -9,3 +9,8 @@ MAGENTA = '\033[35m'
 CYAN    = '\033[36m'
 WHITE   = '\033[37m'
 RESET   = '\033[39m'
+
+def RandomColors():
+    Colors = random.choice([BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE])
+    return Colors
+print(f"{RandomColors()}Testing{RESET}")
