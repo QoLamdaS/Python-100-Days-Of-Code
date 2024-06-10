21310331b3ec7f9aaf5088e7f2ea47f8d269c927
NewBie=) <162775533+QoLamdaS@users.noreply.github.com>
2024-06-10 13:59:46 +0700

    Added Code

---
+++ 100-Days-Of-Code/2-100-User Input.py
@@ -0,0 +1,16 @@
+print("Aloha")
+
+a = input("Your name? ")
+b = input("Your favorite food? ")
+c = input("Your favorite music? ")
+d = input("Your live? ")
+
+print("Getting to know you!")
+
+print("You are")
+print(a)
+print("You're probably hungry for")
+print(b)
+print("and you're definitely getting your groove on to")
+print(c)
+print(d)
