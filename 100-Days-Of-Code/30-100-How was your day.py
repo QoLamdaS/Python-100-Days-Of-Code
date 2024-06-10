aa52b65767b3637be5291566598758332925c961
NewBie=) <162775533+QoLamdaS@users.noreply.github.com>
2024-06-10 21:48:16 +0700

    Testing Only

--- 100-Days-Of-Code/30-100-How was your day.py
+++ 100-Days-Of-Code/30-100-How was your day.py
@@ -2,3 +2,4 @@ print("30 Days Down - How was your day?\n")
 for i in range(1, 31):
     UserInput = input(f"Day {i <3} out of 30: ")
     print(f"You thought Day {i <3} out of 30 was {UserInput}")
+    #! Only Testing
