0ab59530a24acd965c1ed3e05df052e34bd31aac
NewBie=) <162775533+QoLamdaS@users.noreply.github.com>
2024-06-10 13:46:01 +0700

    Code added

---
+++ 1-100-print().py
@@ -0,0 +1,7 @@
+print("Henry")
+print("30/11/1998")
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+print("I am feeling 🗿")
+print("You can follow my progress at replit.com/@LampWay")
