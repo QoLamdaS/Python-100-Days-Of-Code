9fad8b4979eb4f91733148b83fddae86ac957eb5
NewBie=) <162775533+QoLamdaS@users.noreply.github.com>
2024-06-13 23:55:32 +0700

    Completed the program: 1. Make while True loop 2. Make the input for choosing, adding item, remove item, edit item, view the list, and exit the program 3. Make if-statement for each user chooses (add, remove, edit, view, exit) 4. Make nested if-statement in the remove and edit statements

--- 100-Days-Of-Code/33-100-To do lists Manager.py
+++ 100-Days-Of-Code/33-100-To do lists Manager.py
@@ -1,5 +1,37 @@
-ToDoLists = []
+AllLists = []
 
 def ToDoLists():
-    for item in ToDoLists:
-        print(f"\n{item}\n")
+    print("\nTo Do List(s):")
+    for item in AllLists:
+        print(item)
+    print()
+
+while True:
+    UserLists = input("Choose one: 'add', 'remove', 'edit', 'view', or 'exit'? ")
+    if UserLists == "view":
+        print(f"Here is your TO DO LISTS:{AllLists}")
+    elif UserLists == "add":
+        item = input("Add: ")
+        AllLists.append(item) 
+    elif UserLists == "remove":
+        item = input("Remove: ")
+        if item in AllLists:
+            AllLists.remove(item)
+        else:
+            print(f"{item} is not in the list.")
+    elif UserLists == "edit":
+        item = input(f"\n{AllLists}\nWhich one do you want to edit: ")
+        if item in AllLists:
+            AllLists.remove(item)
+            item = input("Edit: ")
+            AllLists.append(item)
+        else:
+            print(f"{item} is not in the list.")
+    elif UserLists == "exit":
+        print("\nThanks for using this program.\nPlease give me some feedbacks to improve my programming skill.\n")
+        quit()
+    else:
+        print("\nINVALID INPUT, Try again!\n")
+        exit()
+    
+    ToDoLists()
