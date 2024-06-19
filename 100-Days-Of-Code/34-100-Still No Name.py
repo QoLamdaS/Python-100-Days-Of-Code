18da7b87ffd7533e1e88c82ec907153576922a93
WillyS <kaffeeunused@gmail.com>
2024-06-19 17:17:21 +0700

    Completed some features : 1. Add email 2. Remove Email 3. View the email - Use While True Loop - Use If-statements and nested if-statement inside the While True Loop - Use input() function inside the While True Loop and if-statements

--- 100-Days-Of-Code/34-100-Still No Name.py
+++ 100-Days-Of-Code/34-100-Still No Name.py
@@ -4,7 +4,29 @@ AllEmailLists = []
 
 def ListsEmail():
     print("\nLists of Email(s)\n")
+    counter = 0
     for Email in AllEmailLists:
         counter += 1
         print(f"{counter}: {Email}")
 
+while True:
+    print("SPAMMER Inc.\n")
+    UserInput = input("1. Add Email\n2. Remove Email\n3. Show Email(s)\n4. Get SPAMMING\n\nUser Input: ")
+    if UserInput == "1":
+        Email = input("New Email: ")
+        AllEmailLists.append(Email)
+    elif UserInput == "2":
+        Email = input("Remove Email: ")
+        if Email in AllEmailLists:
+            AllEmailLists.remove(Email)
+        else:
+            print(f"{Email} isn't in the list.")
+    elif UserInput == "3":
+        print("\nHere is the list of all emails: ")
+        ListsEmail()
+    elif UserInput == "4":
+        print("UNFINISHED")
+        #! Unfinished, still working for this
+    else:
+        print("\nINVALID INPUT!!!\n")
+        exit()
