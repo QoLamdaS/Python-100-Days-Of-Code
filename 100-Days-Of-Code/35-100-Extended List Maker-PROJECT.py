51420c8b1f0584d4e8da8815fb958fe5f98ba407
WillyS <kaffeeunused@gmail.com>
2024-07-02 22:01:53 +0700

    Completed the user input for Add, View, Edit, Remove, and Reset : - make while true loop - make view, add, edit, remove, and reset If-Statement inside the while true loop - make some nested if-statements inside  remove and edit if statement Etc : - Some changes in the ToDoLists Function

--- 100-Days-Of-Code/35-100-Extended List Maker-PROJECT.py
+++ 100-Days-Of-Code/35-100-Extended List Maker-PROJECT.py
@@ -4,6 +4,43 @@ AllList = []
 
 def ToDoLists():
     Number = 0
-    for X in AllList:
+    for Todo in AllList:
         Number += 1
-        print(f"No. {Number}: {AllList}")
+        print(f"No. {Number}: {Todo}")
+
+while True:
+    print("\nExtended TODO List Manager\n")
+    UserInput = input("Do you want to 'view', 'add', 'edit', or 'remove' an item from the to do list or Delete all the list 'reset'? ")
+    if UserInput == "view":
+        ToDoLists()
+    elif UserInput == "add": #! NEED TO ADD 'DO NOT ADD DUPLICATES'
+        Todo = str(input("Add: "))
+        AllList.append(Todo)
+    elif UserInput == "remove":
+        Todo = str(input(f"{AllList}\nRemove: "))
+        if Todo in AllList:
+            UserInput = str(input(f"Are you sure you want to remove '{Todo}' from the list? "))
+            if UserInput == "yes" or UserInput == "Yes" or UserInput == "YES" or UserInput == "y" or UserInput == "Y":
+                AllList.remove(Todo)
+            elif UserInput == "no" or UserInput == "No" or UserInput == "NO" or UserInput == "n" or UserInput == "N":
+                print(f"You cancelled to remove '{Todo}' from the list\n")
+            else:
+                print("Invalid input. Please try again.\n")
+                exit()
+        else:
+            print(f"{Todo} is not in the list.")
+    elif UserInput == "edit":
+        Todo = str(input(f"\n{AllList}\nWhich one do you want to edit: "))
+        if Todo in AllList:
+            print(Todo)
+            AllList.remove(Todo)
+            Todo = str(input("Edit: "))
+            AllList.append(Todo)
+        else:
+            print(f"{Todo} is not in the list.")
+    elif UserInput == "reset":
+        AllList.clear()
+        print("\nAll TODO(s) have been deleted from the list.\n")
+    else:
+        print("Invalid input. Please try again.\n")
+        quit()
