63afc12b08902fa7d790fd25b6ef58e7c75c96de
WillyS <kaffeeunused@gmail.com>
2024-07-04 23:20:25 +0700

    Added IMPORTANT NOTES

---
+++ 100-Days-Of-Code/36-100-List of People's Names.py
@@ -0,0 +1,15 @@
+
+# * Important Note *
+x = "  i love PiZZa"
+print(x.capitalize()) #! It's just capitalize the white space only # " " <<< ONLY CAPITALIZE THAT and another words are lowercase
+y = "hello gUYs"
+print(y.capitalize())
+z = "   heLLo BRO CoDe  "
+print(z.strip().capitalize())
+#! NOT: print(z.capitalize().strip()) # Order matters
+
+#* .lower() = all letters are lower case
+#* .upper() = all letters are UPPER CASE
+#* .title() = capital letter are for the first letter of Every Word
+#! .capitalize() = capital letter for the first letter of only the FIRST word. And the another words are transform to lowercase
+
