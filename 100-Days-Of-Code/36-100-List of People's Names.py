a8dafff9655336bb87aee343ec352fb17c2264b7
WillyS <kaffeeunused@gmail.com>
2024-07-05 17:09:55 +0700

    Add If-Statement, strip() and capitalize() at Input, and drop print with strip() and capitalize() Make If-Statement to prevent duplicates from user Delete print with strip() and capitalize() because it isn't needed and replaced to Input Variables

--- 100-Days-Of-Code/36-100-List of People's Names.py
+++ 100-Days-Of-Code/36-100-List of People's Names.py
@@ -1,8 +1,12 @@
 List_Names = []
 
 while True:
-    First_Name = input("First Name: ")
-    Last_Name = input("Last Name: ")
-
-    print(f"{First_Name.strip().capitalize()} {Last_Name.strip().capitalize()}")
+    First_Name = input("First Name: ").strip().capitalize()
+    Last_Name = input("Last Name: ").strip().capitalize()
+    FullName = f"{First_Name} {Last_Name}"
+    if FullName in List_Names:
+        print(f"ERROR: DUPLICATE!!!{FullName}")
+    else:
+        List_Names.append(FullName)
+        print(FullName)
 
