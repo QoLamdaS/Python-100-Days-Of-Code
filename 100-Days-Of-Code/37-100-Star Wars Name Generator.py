69ca055af13927dc46ebab7921972e5838954a39
WillyS <kaffeeunused@gmail.com>
2024-07-05 20:24:26 +0700

    Add while True loop at the first line To make sure the program always run

--- 100-Days-Of-Code/37-100-Star Wars Name Generator.py
+++ 100-Days-Of-Code/37-100-Star Wars Name Generator.py
@@ -1,8 +1,9 @@
-print("\nSTARS WARS NAME GENERATOR\n")
+while True:
+    print("\nSTARS WARS NAME GENERATOR\n")
+    
+    FirstName = input("First Name: ").strip().capitalize()
+    LastName = input("Last Name: ").strip().lower()
+    Mom_Maiden = input("Mom's Maiden Name: ").strip().capitalize()
+    TheCity = input("City Name: ").strip().lower()
 
-FirstName = input("First Name: ").strip().capitalize()
-LastName = input("Last Name: ").strip().lower()
-Mom_Maiden = input("Mom's Maiden Name: ").strip().capitalize()
-TheCity = input("City Name: ").strip().lower()
-
-print(f"\nYour Star Wars name is {FirstName[:3:]}{LastName[:3:]} {Mom_Maiden[:2:]}{TheCity[4::]}\n")
+    print(f"\nYour Star Wars name is {FirstName[:3:]}{LastName[:3:]} {Mom_Maiden[:2:]}{TheCity[4::]}\n")
