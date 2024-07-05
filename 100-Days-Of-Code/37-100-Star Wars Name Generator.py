7bf6cec0ec2f5d7772f0c661a9e9b03c57ddcfe7
WillyS <kaffeeunused@gmail.com>
2024-07-05 18:04:45 +0700

    Add : four variables input

--- 100-Days-Of-Code/37-100-Star Wars Name Generator.py
+++ 100-Days-Of-Code/37-100-Star Wars Name Generator.py
@@ -1,2 +1,7 @@
 print("\nSTARS WARS NAME GENERATOR\n")
 
+FirstName = input("First Name: ")
+LastName = input("Last Name: ")
+Mom_Maiden = input("Mom's Maiden Name: ")
+TheCity = input("City Name: ")
+
