fb72bbf62bc482a1981819baf2ee0ac94741deae
WillyS <kaffeeunused@gmail.com>
2024-07-05 16:27:50 +0700

    Add : while True loop at the top Reason : To make endless loop

--- 100-Days-Of-Code/36-100-List of People's Names.py
+++ 100-Days-Of-Code/36-100-List of People's Names.py
@@ -1,8 +1,8 @@
-
 List_Names = []
 
-First_Name = input("First Name: ")
-Last_Name = input("Last Name: ")
+while True:
+    First_Name = input("First Name: ")
+    Last_Name = input("Last Name: ")
 
-print(f"{First_Name.strip().capitalize()} {Last_Name.strip().capitalize()}")
+    print(f"{First_Name.strip().capitalize()} {Last_Name.strip().capitalize()}")
 
