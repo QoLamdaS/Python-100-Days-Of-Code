e7f6f5081b0a9cdda0000842ef5337b295dc5778
WillyS <kaffeeunused@gmail.com>
2024-07-05 19:56:22 +0700

    Add strip() and capitalize() for the four input variables To make sure the name input is well organized and neat

--- 100-Days-Of-Code/37-100-Star Wars Name Generator.py
+++ 100-Days-Of-Code/37-100-Star Wars Name Generator.py
@@ -1,7 +1,7 @@
 print("\nSTARS WARS NAME GENERATOR\n")
 
-FirstName = input("First Name: ")
-LastName = input("Last Name: ")
-Mom_Maiden = input("Mom's Maiden Name: ")
-TheCity = input("City Name: ")
+FirstName = input("First Name: ").strip().capitalize()
+LastName = input("Last Name: ").strip().capitalize()
+Mom_Maiden = input("Mom's Maiden Name: ").strip().capitalize()
+TheCity = input("City Name: ").strip().capitalize()
 
