da396109f575b5e74c460117e90bc6abf556b21f
WillyS <kaffeeunused@gmail.com>
2024-07-05 20:20:09 +0700

    Add print the final name generate from the user input

--- 100-Days-Of-Code/37-100-Star Wars Name Generator.py
+++ 100-Days-Of-Code/37-100-Star Wars Name Generator.py
@@ -5,5 +5,4 @@ LastName = input("Last Name: ").strip().lower()
 Mom_Maiden = input("Mom's Maiden Name: ").strip().capitalize()
 TheCity = input("City Name: ").strip().lower()
 
-
-
+print(f"\nYour Star Wars name is {FirstName[:3:]}{LastName[:3:]} {Mom_Maiden[:2:]}{TheCity[4::]}\n")
