e5e78a1b47be7359f56a871a085d06fe139749a4
WillyS <kaffeeunused@gmail.com>
2024-07-05 20:16:19 +0700

    Replace strip() to lower() for LastName and TheCity variables

--- 100-Days-Of-Code/37-100-Star Wars Name Generator.py
+++ 100-Days-Of-Code/37-100-Star Wars Name Generator.py
@@ -1,7 +1,9 @@
 print("\nSTARS WARS NAME GENERATOR\n")
 
 FirstName = input("First Name: ").strip().capitalize()
-LastName = input("Last Name: ").strip().capitalize()
+LastName = input("Last Name: ").strip().lower()
 Mom_Maiden = input("Mom's Maiden Name: ").strip().capitalize()
-TheCity = input("City Name: ").strip().capitalize()
+TheCity = input("City Name: ").strip().lower()
+
+
 
