b663d7723e3b23c3c1db03b3ca9a5fd9aa2fde5b
WillyS <kaffeeunused@gmail.com>
2024-07-07 22:57:34 +0700

    Important notes about FOR LOOPS

--- 100-Days-Of-Code/38-100-Rainbow Ising.py
+++ 100-Days-Of-Code/38-100-Rainbow Ising.py
@@ -1,2 +1,19 @@
 
-#! Testing???
+#* Important Notes
+
+#! This is a For Loop
+#for i in "BanaNA":
+    #print(i.lower())
+
+YELLOW = '\033[33m'
+END = '\033[0m'
+
+myString = "An apple and a banAnA"
+for letter in myString:
+    if letter.lower() == "a": #! Alternative : if letter.upper() == "A":
+    #? Why the all letters not transformed to the lowercase?
+    #* Because it's not at the print() and letter.lower() is at the If-Statement
+        print(YELLOW, end = '')
+    print(letter)
+    print(END, end = '')
+
