695d208a10253cc2b62fd4ca9bba9e52d2c94773
QoLamdaS <kaffeeunused@gmail.com>
2024-07-14 00:10:35 +0700

    Typo fixed, end parameter still unclear and confusing

--- 100-Days-Of-Code/38-100-Rainbow Ising.py
+++ 100-Days-Of-Code/38-100-Rainbow Ising.py
@@ -11,7 +11,7 @@ END = '\033[0m'
 myString = "An apple and a banAnA"
 for letter in myString:
     if letter.lower() == "a": #! Alternative : if letter.upper() == "A":
-    #? Why the all letters not transformed to the lowerNase?
+    #? Why the all letters not transformed to the lowerCase?
     #* Because it's not at the print() and letter.lower() is at the If-Statement
         print(YELLOW, end = "")
     print(letter, end = "") #* end = "" >>> LINK : https://www.toppr.com/guides/python-guide/questions/what-does-end-do-in-python/#:~:text=The%20end%20parameter%20in%20the,whitespace%20and%20not%20a%20newline.
