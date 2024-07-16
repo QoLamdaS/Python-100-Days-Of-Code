d5be71655b59193a96d671e7214f9f4ae4f0f2d8
WillyS <162775533+QoLamdaS@users.noreply.github.com>
2024-07-16 12:24:08 +0000

    Delete the Testing comments

--- 100-Days-Of-Code/38-100-Rainbow Ising.py
+++ 100-Days-Of-Code/38-100-Rainbow Ising.py
@@ -3,7 +3,7 @@ RED = "\033[0;31m"
 YELLOW = "\033[1;33m"
 GREEN = "\033[0;32m"
 END = "\033[0m"
-# Testing Only #Test part 2
+
 print("\nRainbow-Ising\n")
 SentenceInput = input("What sentence do you want to RAINBOW-ISING? ")
 
