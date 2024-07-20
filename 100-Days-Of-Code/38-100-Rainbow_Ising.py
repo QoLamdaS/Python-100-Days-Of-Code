02f243aa0fcd41db268cf6923953291395b2ffee
QoLamdaS <kaffeeunused@gmail.com>
2024-07-20 23:49:06 +0700

    Rename the file

---
+++ 100-Days-Of-Code/38-100-Rainbow_Ising.py
@@ -0,0 +1,9 @@
+BLUE = "\033[0;34m"
+RED = "\033[0;31m"
+YELLOW = "\033[1;33m"
+GREEN = "\033[0;32m"
+END = "\033[0m"
+print("TESTING ONLY")
+print("\nRainbow-Ising\n")
+SentenceInput = input("What sentence do you want to RAINBOW-ISING? ")
+
