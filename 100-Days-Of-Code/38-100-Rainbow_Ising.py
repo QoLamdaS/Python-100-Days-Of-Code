3ce025bb04849208b1d47ec26bbbc49c492eaadf
WillyS <162775533+QoLamdaS@users.noreply.github.com>
2024-07-22 00:37:03 +0000

    Add : Time and Os library
    To make the User Interface more neat and organize.

--- 100-Days-Of-Code/38-100-Rainbow_Ising.py
+++ 100-Days-Of-Code/38-100-Rainbow_Ising.py
@@ -1,9 +1,13 @@
+import os, time
+
 BLUE = "\033[0;34m"
 RED = "\033[0;31m"
 YELLOW = "\033[1;33m"
 GREEN = "\033[0;32m"
 END = "\033[0m"
 
+os.system('clear') #os.system('cls') #! Use this for Windows OS
 print("\nRainbow-Ising\n")
+time.sleep(3)
 SentenceInput = input("What sentence do you want to RAINBOW-ISING? ")
 
