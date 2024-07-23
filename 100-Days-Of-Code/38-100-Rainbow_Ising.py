4b84f3d1a1043d852d9c4200b0c4e375d6850fb3
WillyS <162775533+QoLamdaS@users.noreply.github.com>
2024-07-23 07:32:05 +0000

    Notes added

--- 100-Days-Of-Code/38-100-Rainbow_Ising.py
+++ 100-Days-Of-Code/38-100-Rainbow_Ising.py
@@ -1,10 +1,20 @@
-BLUE = "\033[0;34m"
-RED = "\033[0;31m"
-YELLOW = "\033[1;33m"
-GREEN = "\033[0;32m"
-END = "\033[0m"
-print("TESTING ONLY")
-print("\nRainbow-Ising\n")
-SentenceInput = input("What sentence do you want to RAINBOW-ISING? ")
 
-#! Testing AGAIN
+#! This is a For Loop
+#for i in "BanaNA":
+    #print(i.lower())
+
+YELLOW = '\033[33m'
+END = '\033[0m'
+
+myString = "An apple and a banAnA"
+for letter in myString:
+    if letter.lower() == "a": #! Alternative : if letter.upper() == "A":
+    #? Why the all letters not transformed to the lowerCase?
+    #* Because it's not at the print() and letter.lower() is at the If-Statement
+        print(YELLOW, end = "")
+    print(letter, end = "") #* end = "" >>> LINK : https://www.toppr.com/guides/python-guide/questions/what-does-end-do-in-python/#:~:text=The%20end%20parameter%20in%20the,whitespace%20and%20not%20a%20newline.
+    print(END, end = "")
+
+#! This is End parameter for print
+print("\n\nThis is the first line", end = "")
+print("SECOND LINE")
