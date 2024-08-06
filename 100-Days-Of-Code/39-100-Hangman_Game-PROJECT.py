ab70d58ef0b771c1ddb4bcf13126629bf7d6d623
WillyS <162775533+QoLamdaS@users.noreply.github.com>
2024-08-06 00:21:01 +0000

    Add the List of words

--- 100-Days-Of-Code/39-100-Hangman_Game-PROJECT.py
+++ 100-Days-Of-Code/39-100-Hangman_Game-PROJECT.py
@@ -2,6 +2,8 @@ import random
 import os
 import time
 
+ListOfWords = ["british", "suave", "integrity", "accent", "evil", "gen", "downtown", "downstairs", "streamline", "funnel"]
+
 os.system('clear') #os.system('cls') #For Windows OS
 print("\nHangman Game\n")
 time.sleep(3)
