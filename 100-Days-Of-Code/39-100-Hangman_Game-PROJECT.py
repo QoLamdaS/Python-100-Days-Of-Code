a6896c0cb0a3d9263e4d6d872ca6ed1d252eaef5
QoLamdaS <kaffeeunused@gmail.com>
2024-08-08 22:17:22 +0700

    Source to learn random word generator Function

---
+++ 100-Days-Of-Code/39-100-Hangman_Game-PROJECT.py
@@ -0,0 +1,16 @@
+import random
+import os
+import time
+
+ListOfWords = ["british", "suave", "integrity", "accent", "evil", "gen", "downtown", "downstairs", "streamline", "funnel"]
+def ChosenWord(): #! Sources to learn more: My day 27 and day 28 Replit PROJECT DAY
+    The_Chosen_Word = random.choice(ListOfWords)
+    return The_Chosen_Word
+
+while True:
+    os.system('cls') #os.system('cls') Windows OS #os.system('clear') Linux and Mac OS
+    Chosen_Answer = ChosenWord()
+    print("\nHangman Game\n")
+    time.sleep(3)
+    LetterInput = input("Choose a letter: ")
+
