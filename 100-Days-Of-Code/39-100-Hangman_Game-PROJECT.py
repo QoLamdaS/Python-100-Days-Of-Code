6c179f102fca8d5a12c4ffba92abe12ae09fff6d
QoLamdaS <kaffeeunused@gmail.com>
2024-08-08 22:13:09 +0700

    Add a function to randomize the word

--- 100-Days-Of-Code/39-100-Hangman_Game-PROJECT.py
+++ 100-Days-Of-Code/39-100-Hangman_Game-PROJECT.py
@@ -3,9 +3,13 @@ import os
 import time
 
 ListOfWords = ["british", "suave", "integrity", "accent", "evil", "gen", "downtown", "downstairs", "streamline", "funnel"]
+def ChosenWord():
+    The_Chosen_Word = random.choice(ListOfWords)
+    return The_Chosen_Word
 
 while True:
-    os.system('clear') #os.system('cls') #For Windows OS
+    os.system('cls') #os.system('cls') Windows OS #os.system('clear') Linux and Mac OS
+    Chosen_Answer = ChosenWord()
     print("\nHangman Game\n")
     time.sleep(3)
     LetterInput = input("Choose a letter: ")
