8b4be426e72575cc3cbdc10808924154f1187086
QoLamdaS <kaffeeunused@gmail.com>
2024-08-11 10:42:05 +0700

    Reformat the Chosen_Answer variable

--- 100-Days-Of-Code/39-100-Hangman_Game-PROJECT.py
+++ 100-Days-Of-Code/39-100-Hangman_Game-PROJECT.py
@@ -6,10 +6,10 @@ ListOfWords = ["british", "suave", "integrity", "accent", "evil", "gen", "downto
 def ChosenWord():
     The_Chosen_Word = random.choice(ListOfWords)
     return The_Chosen_Word
+Chosen_Answer = ChosenWord()
 
 while True:
     os.system('cls') #os.system('cls') Windows OS #os.system('clear') Linux and Mac OS
-    Chosen_Answer = ChosenWord()
     print("\nHangman Game\n")
     time.sleep(3)
     LetterInput = input("Choose a letter: ").lower()
