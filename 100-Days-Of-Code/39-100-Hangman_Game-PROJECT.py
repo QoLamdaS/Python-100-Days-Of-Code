05506794e7c71460c25fc340e5cd500e44addb12
WillyS <162775533+QoLamdaS@users.noreply.github.com>
2024-08-12 00:04:09 +0000

    Add Already_Picked_Letter variable list
    To prevent user to pick the same letter

--- 100-Days-Of-Code/39-100-Hangman_Game-PROJECT.py
+++ 100-Days-Of-Code/39-100-Hangman_Game-PROJECT.py
@@ -3,14 +3,14 @@ import os
 import time
 
 ListOfWords = ["british", "suave", "integrity", "accent", "evil", "gen", "downtown", "downstairs", "streamline", "funnel"]
+Already_Picked_Letter = []
 def ChosenWord():
     The_Chosen_Word = random.choice(ListOfWords)
     return The_Chosen_Word
 Chosen_Answer = ChosenWord()
 
 while True:
-    os.system('cls') #os.system('cls') Windows OS #os.system('clear') Linux and Mac OS
+    os.system('clear') #os.system('cls') Windows OS #os.system('clear') Linux and Mac OS
     print("\nHangman Game\n")
     time.sleep(3)
     LetterInput = input("Choose a letter: ").lower()
-
