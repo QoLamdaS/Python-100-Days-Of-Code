1dc658be80374fb8e53698b548c9b68d62931a99
WillyS <162775533+QoLamdaS@users.noreply.github.com>
2024-08-22 03:50:00 +0000

    Print the dictionaries and fix the issue for integers and strings in variable input
    Add str() to all variables input to ensure the inputs are strings, NOT INTEGERS

--- 100-Days-Of-Code/40-100-Contact_Card.py
+++ 100-Days-Of-Code/40-100-Contact_Card.py
@@ -1,12 +1,14 @@
 import os
 
-os.system('cls')
+os.system('clear') #os.system('cls') for Windows #os.system('clear') for Linux and Mac
 print("\nContact Card\n")
-InputName = input("Name > ")
-InputD_O_B = input("Date of Birth > ")
-InputTelephone_Number = input("Telephone Number > ")
-InputEmail = input("Email > ")
-InputAddress = input("Address > ")
+InputName = str(input("Name > "))
+InputD_O_B = str(input("Date of Birth > "))
+InputTelephone_Number = str(input("Telephone Number > "))
+InputEmail = str(input("Email > "))
+InputAddress = str(input("Address > "))
 
 The_Dictionaries = {'NAME':InputName, 'DOB':InputD_O_B, 'TEL':InputTelephone_Number, 'EMAIL':InputEmail, 'ADDRESS':InputAddress}
 
+os.system('clear') #os.system('cls') for Windows #os.system('clear') for Linux and MacOS
+print(f"\nHere's your contact card\n\nName: {The_Dictionaries['NAME']}\nDate of Birth: {The_Dictionaries['DOB']}\nTelephone Number: {The_Dictionaries['TEL']}\nEmail: {The_Dictionaries['EMAIL']}\nAddress: {The_Dictionaries['ADDRESS']}")
