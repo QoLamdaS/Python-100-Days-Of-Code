f7137553c745d2e50f3e6776703225e3e2e89fef
WillyS <162775533+QoLamdaS@users.noreply.github.com>
2024-08-22 02:49:59 +0000

    Add necessary variable input

--- 100-Days-Of-Code/40-100-Contact_Card.py
+++ 100-Days-Of-Code/40-100-Contact_Card.py
@@ -2,3 +2,8 @@ import os
 
 os.system('cls')
 print("\nContact Card\n")
+InputName = input("Name > ")
+InputD_O_B = input("Date of Birth > ")
+InputTelephone_Number = input("Telephone Number > ")
+InputEmail = input("Email > ")
+InputAddress = input("Address > ")
