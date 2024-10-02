fa8d9b85a000f527022085097fe2f7c8884b3e03
QoLamdaS <kaffeeunused@gmail.com>
2024-10-02 21:18:48 +0700

    Add variables input

--- 100-Days-Of-Code/41-100-Website_StoreKeys.py
+++ 100-Days-Of-Code/41-100-Website_StoreKeys.py
@@ -1,1 +1,5 @@
-print("\nTesting Only\n")
+NameInput = str(input("Enter name: "))
+UrlInput = str(input("Enter URL: "))
+DescInput = str(input("Enter description: "))
+RatingInput = str(input("Enter rating: "))
+
