521ddb424ace8c1c2408d7d45f50f2d2d1c0b59c
QoLamdaS <kaffeeunused@gmail.com>
2024-10-07 22:03:03 +0700

    Add os and time library

--- 100-Days-Of-Code/41-100-Website_StoreKeys.py
+++ 100-Days-Of-Code/41-100-Website_StoreKeys.py
@@ -1,7 +1,13 @@
+import os, time
+
+os.system('cls')
 NameInput = str(input("Enter name: "))
 UrlInput = str(input("Enter URL: "))
 DescInput = str(input("Enter description: "))
 RatingInput = str(input("Enter rating: "))
+
+time.sleep(5)
+os.system('cls')
 
 TheDictionary = {"Name": NameInput, "URL": UrlInput, "Description": DescInput, "Rating": RatingInput}
 
