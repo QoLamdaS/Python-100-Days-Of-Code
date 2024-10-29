3cf20d304ae78bbc8bf00721758c12da3c90335d
QoLamdaS <kaffeeunused@gmail.com>
2024-10-29 21:56:58 +0700

    Add variables input

--- 100-Days-Of-Code/42-100-MokeBeast_Pokemon-PROJECT.py
+++ 100-Days-Of-Code/42-100-MokeBeast_Pokemon-PROJECT.py
@@ -2,3 +2,9 @@ import os
 
 os.system('cls')
 print("MokeBeast Pokemon\n")
+BeastInput = str(input("Beast name: "))
+TypeInput = str(input("Type: "))
+SpecialMoveInput = str(input("Special move: "))
+HPinput = str(input("HP: "))
+Mp_Input = str(input("MP: "))
+
